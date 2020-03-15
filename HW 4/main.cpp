7db9fb7e7bb1010fd10423bead931477a1985dd2
Zachary Chang <zachang890@gmail.com>
2020-03-14 22:07:17 -0700

    Add files via upload

---
+++ HW 4/main.cpp
@@ -0,0 +1,46 @@
+//
+//  main.cpp
+//  CS 32 Homework 4 Zachary Chang
+//
+//  Created by Zachary Chang on 2/12/20.
+//  Copyright © 2020 Zachary Chang. All rights reserved.
+//
+
+#include "Map.h"
+#include <iostream>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+void test()
+{
+    Map<int, double> mid;
+    Map<string, int> msi;
+    assert(msi.empty());
+    assert(msi.size() == 0);
+    assert(msi.insert("Hello", 10));
+    assert(mid.insert(10, 3.5));
+    assert(msi.update("Hello", 20));
+    assert(mid.update(10, 4.75));
+    assert(msi.insertOrUpdate("Goodbye", 30));
+    assert(msi.erase("Goodbye"));
+    assert(mid.contains(10));
+    int k;
+    assert(msi.get("Hello", k));
+    string s;
+    assert(msi.get(0, s, k));
+    Map<string, int> msi2(msi);
+    msi2.swap(msi);
+    msi2 = msi;
+    combine(msi,msi2,msi);
+    combine(mid,mid,mid);
+    reassign(msi,msi2);
+    reassign(mid,mid);
+}
+
+int main()
+{
+    test();
+    cout << "Passed all tests" << endl;
+}
